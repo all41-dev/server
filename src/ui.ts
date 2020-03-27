10bfe9ba578f9b5fe07aada56ebc7a9f6e63819b
Eric Berger <eric@harps.ch>
2020-03-27 09:09:45 +0100

    fix(Ui): upd getBaseRouter signature remove options which became a class member

--- src/ui.ts
+++ src/ui.ts
@@ -19,19 +19,19 @@ export abstract class Ui<T extends Ui<T>> {
     this.router = this.createRouter();
   }
 
-  public getBaseRouter(dir: string, options: IUiOptions<T>): express.Router {
+  public getBaseRouter(dir: string): express.Router {
 
     // load angular config to resolve default project
     const angularConfig = require(path.resolve(dir, '../angular.json'));
 
     // return configuration
     this.router.use('/_config', (_req: express.Request, res: express.Response): express.Response => {
-      return res.json(options.config || {});
+      return res.json(this._options.config || {});
     });
 
     // read dist path with help of angular config
     const p = path.resolve(dir, './' + angularConfig.defaultProject);
-    if (options.config && (options.config as any).requireAuth) { this.router.use('/', requiresAuth(), express.static(p)); }
+    if (this._options.config && (this._options.config as any).requireAuth) { this.router.use('/', requiresAuth(), express.static(p)); }
     else { this.router.use('/', express.static(p)); }
 
     // add router to provided application
