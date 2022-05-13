e683b48608709b85693fe7e24aefa0b37f5bfd70
Quentin <quentin@harps.ch>
2022-05-13 14:00:49 +0200

    fix : type fixed

--- src/request-controller.ts
+++ src/request-controller.ts
@@ -9,7 +9,7 @@ import { Model } from 'sequelize-typescript';
  */
 export abstract class RequestController<T extends EntityRequest<Model, any>> extends ControllerBase {
   private _requestType: new () => T
-  public constructor(entityType: new (dbType? : new(t1? : Model) => Model) => T) {
+  public constructor(entityType: new (dbType? : new(t1? : any) => any) => T) {
     super();
     this._requestType = entityType;
   }
