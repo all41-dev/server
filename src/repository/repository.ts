161502e284e552efa0625813afd22a2e97bee325
Eric Berger <eric@harps.ch>
2023-12-04 11:17:03 +0100

    feat(Repository): adding default for generic type

--- src/repository/repository.ts
+++ src/repository/repository.ts
@@ -1,9 +1,9 @@
 export interface IPkName<T> { readonly pkName: keyof T }
-export interface IRepositoryReadable<T extends IPkName<T>> {
+export interface IRepositoryReadable<T extends IPkName<T> = any> {
   getByKey(key: any, options?: any): Promise<T>;
   get(options: any): Promise<T[]>;
 }
-export interface IRepositoryWritable<T extends IPkName<T>> {
+export interface IRepositoryWritable<T extends IPkName<T> = any> {
   patch(key: any, object: Partial<T>, options?: any): Promise<T>;
   post(object: T, options?: any): Promise<T>;
   delete(key: any): Promise<void>;
@@ -12,6 +12,6 @@ export interface IRepositoryWritable<T extends IPkName<T>> {
 /**
  * @template T model type served by the repository
  */
-export interface Repository<T extends IPkName<T>> {
+export interface Repository<T extends IPkName<T> = any> {
   readonly modelType: new (plainObj?: Partial<T>) => T;
 }
