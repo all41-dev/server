c32f9c62e1c64c7c5442421c4f495df5d812bc3a
Radmyr <radmyr.deriabin11@gmail.com>
2024-01-22 09:03:05 +0100

    feat(repo-seq): crud implemented

--- src/repository/repository-sequelize.ts
+++ src/repository/repository-sequelize.ts
@@ -7,9 +7,9 @@ import { Utils } from "../utils";
 export class RepositorySequelize<T extends Model<T> & IPkName<T>> implements Repository<T>, IRepositoryReadable<T>, IRepositoryWritable<T> {
   public readonly modelType: new (values?: SQLUtils.MakeNullishOptional<T> | Partial<T>, options?: BuildOptions) => T;
   public readonly dbName?: string;
-  
+
   /**
-   * 
+   *
    * @param type Model type
    * @param dbName database name (from Server.dbs.dbName value)
    */
@@ -22,11 +22,16 @@ export class RepositorySequelize<T extends Model<T> & IPkName<T>> implements Rep
     if (!sequelizeRepository) throw new Error(`Repository for '${this.modelType.prototype.constructor.name}' not found`);
     return sequelizeRepository;
   }
-  post(object: T): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async post(object: T): Promise<T> {
+    return await this._sequelizeRepository.create(object as any);
   }
-  delete(key: any): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async delete(key: any): Promise<void> {
+    const recordToDelete = await this.getByKey(key);
+    if (recordToDelete) {
+      await recordToDelete.destroy();
+    } else {
+      throw new Error(`No record found with key '${key}'`);
+    }
   }
 
 
@@ -35,7 +40,7 @@ export class RepositorySequelize<T extends Model<T> & IPkName<T>> implements Rep
 
     const localOptions = {[this._sequelizeRepository.primaryKeyAttribute]: key};
     Object.assign(localOptions, options);
-    const result = await this._sequelizeRepository.findOne(options);
+    const result = await this._sequelizeRepository.findByPk(key);
     if (result === null) throw new Error(`No record found with key '${key} on repository '${this.modelType.constructor.name}`);
     Utils.inst.dateToDateTime(result);
     return result;
@@ -44,7 +49,7 @@ export class RepositorySequelize<T extends Model<T> & IPkName<T>> implements Rep
     // in case repository mode has limitations
     // const result = await this.modelType.findAll(options);
 
-    // const foo: Includeable = { 
+    // const foo: Includeable = {
     //   isAliased: true,
     // };
   
@@ -60,6 +65,6 @@ export class RepositorySequelize<T extends Model<T> & IPkName<T>> implements Rep
     if (options) {
       result = await this.getByKey(options);
     }
-    return result;
+    return result as any;
   }
 }
