43983952a8b94dbe32759a7904e5af2a90f0472f
QGavoille <quentin@harps.ch>
2024-05-29 15:55:05 +0200

    BREAKING CHANGE(workflow): context

--- src/workflow/workflow.ts
+++ src/workflow/workflow.ts
@@ -1,32 +1,31 @@
-import { IPkName, IRepositoryWritable } from "../repository/repository";
+import { IPkName, IRepositoryWritable } from "../repository/repository"
 
 export interface Context {
   source: string;
-  operation: 'post' | 'patch' | 'delete';
-  data: any;
+  data: {record: any, additionnal?: any};
 }
 // export type Context = ContextStd & any;
 
-export interface Workflow<T extends IPkName<T>> {
+export interface Workflow<T extends IPkName<T>, C extends Context = Context> {
   readonly modelType: new (plainObj?: Partial<T>) => T;
   actors: { [key: string]: Actor<T> }
-  actions: { [key: string]: Action<T> };
+  actions: { [key: string]: Action<T, C> };
 }
 
-export class Workflow<T extends IPkName<T>> {
-  public async run(source: string, operation : 'post' | 'patch' | 'delete', data: Partial<T>): Promise<T[]> {
+export class Workflow<T extends IPkName<T>, C extends Context = Context> {
+  public async run(ctx : C): Promise<(T | void)[]> {
     const electedKeys = Object.keys(this.actions)
-      .filter((key) => this.actions[key].condition({ source, operation, data }));
-    const result = await Promise.all(electedKeys.map(async (key) => this.executeAction(this.actions[key], source, operation, data)));
+      .filter((key) => this.actions[key].condition(ctx));
+    const result = await Promise.all(electedKeys.map(async (key) => this.executeAction(this.actions[key], ctx)));
     return result;
   }
-  private async executeAction(action: Action<T>, source: string, operation: 'post' | 'patch' | 'delete', data: any): Promise<T> {
+  private async executeAction(action: Action<T, C>, ctx : C): Promise<T |void> {
     {
       const res = action.doAwait ?
-        await action.execute(data) :
-        action.execute(data);
-      const electedSuccessors = action.successors.filter((successor) => successor.condition({ source, operation, data }));
-      const successorsResult = Promise.all(electedSuccessors.map(async (successor) => this.executeAction(successor, source, operation, data)));
+        await action.execute(ctx.data) :
+        action.execute(ctx.data);
+      const electedSuccessors = action.successors.filter((successor) => successor.condition(ctx));
+      const successorsResult = Promise.all(electedSuccessors.map(async (successor) => this.executeAction(successor, ctx)));
       action.doAwait ? await successorsResult : successorsResult;
       return res;
     }
@@ -37,9 +36,9 @@ export interface Actor<T extends IPkName<T>> {
   repository: IRepositoryWritable<T>;
 }
 
-export interface Action<T> {
-  successors: Action<T>[];
-  condition: (context: Context) => boolean;
-  execute: (data: Partial<T>) => Promise<T>;
+export interface Action<T, C> {
+  successors: Action<T, C>[];
+  condition: (context: C) => boolean;
+  execute: (data: {record: Partial<T>, additionnal?: any}) => Promise<T | void>; // void in case of delete
   doAwait?: boolean;
 }
