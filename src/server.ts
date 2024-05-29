698270b2102c95b9dbbc63d2d136734ed49ddc73
QGavoille <quentin@harps.ch>
2024-05-29 13:54:20 +0200

    feat(amqp): add getter to get all amqp instances

--- src/server.ts
+++ src/server.ts
@@ -155,6 +155,7 @@ export class Server {
     return this._app;
   }
   public get dbs(): Db<any>[] { return this._dbs || []; }
+  public get amqp(): {[key : string] : IAmqpOptions} { return this._amqp; }
 
   public async stop(killProcess = true): Promise<void> {
     if (this.http) {
